3fe17a61eee21cb72065396cf17cf89e9ab247e7
Asriel Nguyen <airdroid271@gmail.com>
2022-11-22 22:53:24 +0700

    updated

--- script.js
+++ script.js
@@ -1,33 +1,40 @@
-// Create and display the div
-var div = document.createElement("div");
-div.innerHTML = '<h1>Hello World!</h1><input type="text" value="#" />';
-document.body.appendChild(div);
+const input = document.querySelector('input');
+const text = document.querySelector('h1');
 
-// Listen to the input box
-div.querySelector("input").addEventListener("input", function(e) {
-    var value = e.target.value
-    if (value.match(/^#[0-9a-f]{6}$/i)) {
-        document.body.style.backgroundColor = value
-        div.querySelector("h1").style.color = getContrastingColor(value)
+input.addEventListener('input', function (e) {
+    var value = e.target.value;
+
+    // Shitty regex to check if the input is a valid hex color
+    // This is not a good way to do this, but if it works, it works :D
+    // The same goes for hexToRgb()
+    if (value.match(/^[0-9a-f]{6}$/i)) {
+        text.style.color = '#' + value;
+        text.style.backgroundColor = '#' + value;
+        text.style.borderColor = '#' + value;
+        text.style.color = getContrastingColor(value);
+        text.innerText = hexToRgb(`#` + value).r + `, ` + hexToRgb(`#` + value).g + `, ` + hexToRgb(`#` + value).b;
+
     } else {
-        document.body.style.backgroundColor = "white"
-        div.querySelector("h1").style.color = "black"
+        text.innerText = `Please enter a valid hex code`;
+        text.style.backgroundColor = `transparent`;
+        text.style.color = `black`;
     }
 });
 
+
 // Functions
+function hexToRgb(hex) {
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex) 
+    return result ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16)
+    } : null
+};
+
 function getContrastingColor(color) {
     var rgb = hexToRgb(color)
     var isLight = rgb.r * 0.299 + rgb.g * 0.587 + rgb.b * 0.114 > 186
     return isLight ? "black" : "white"
-};
-
-function hexToRgb(hex) {
-    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex)
-    return result ? {
-        r: parseInt(result[1], 16),
-        g: parseInt(result[2], 16),
-        b: parseInt(result[3], 16) 
-    } : null
 };
 
